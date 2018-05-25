61515b63ea7f7f6e83c1f5ce7803baf5fdb30596
Vignesh <vigneshb6789@gmail.com>
2018-05-24 20:38:28 -0500

    second commit

--- src/test.java
+++ src/test.java
@@ -4,6 +4,10 @@ public class test {
 	public static void main(String[] args) {
 		
 		System.out.println("Jenkins-github integration");
+		
+		System.out.println("Jenkins-github integration updated");
+		
+		
 	}
 
 }
